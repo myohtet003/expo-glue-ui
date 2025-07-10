a672db4b0aa4a0c1e51f51534ca79a3e3c372259
myohtet003 <myohtetkyaw2003@gmail.com>
2025-07-10 19:00:48 +0630

    start authentication

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,48 +1,20 @@
-import { Text } from 'react-native'
-import {useState, useEffect} from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { Text } from 'react-native' 
+import { SafeAreaView } from 'react-native-safe-area-context' 
+import { useSession } from '../../providers/ctx';
 
-import {base_url } from '@/constants/index';
-
-type User = {
-	id: string,
-	name: string
-};
-
-const fetchUsers = async () => {
-	const response = await fetch(base_url + "users")
-	if(!response.ok) {
-		throw new Error('Fail to load Users')
-	}
-	return response.json();
-}
+ 
 const Profile = () => {
+	const { signOut } = useSession();
 
-	const [users, setUsers] = useState<User[] | null>([]);
-	const [loading, setLoading] = useState(true);
-
-	useEffect(() => {
-		const getUsers = async () => {
-			try{
-				const data = await fetchUsers();
-				setUsers(data)
-			}catch(error){
-				console.error('Error Fetching Users:' , error);
-			}finally{
-				setLoading(false)
-			}
-		}
-		getUsers();
-	}, []);
   return (
 	<SafeAreaView>
-		{loading ? ( 
-			<Text> Loading.....</Text>
-		) : (
-
-			users?.map((user) => <Text key={user.id}>{user.name}</Text>)
-		)}
-	  <Text>Profile</Text> 
+		<Text
+        onPress={() => {
+          // The `app/(app)/_layout.tsx` will redirect to the sign-in screen.
+          signOut();
+        }}>
+        Sign Out
+      </Text>
 	</SafeAreaView>
   )
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,31 +1,56 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
 import "@/global.css";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
+import { SessionProvider,useSession } from "../providers/ctx";
+import { SplashScreenController } from "../providers/splash";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+
+import { useColorScheme } from "@/hooks/useColorScheme";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   if (!loaded) {
     // Async font loading only occurs in development.
     return null;
   }
+  return (
+    <SessionProvider>
+      <SplashScreenController />
+      <RootNavigator />
+    </SessionProvider>
+  );
+}
+
+function RootNavigator() {
+  const colorScheme = useColorScheme();
+  const { session } = useSession();
 
   return (
-    <GluestackUIProvider mode="light"><ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <GluestackUIProvider mode="light">
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
         <Stack>
+          <Stack.Protected  guard={!!session}>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="+not-found" />
+        </Stack.Protected>
+          <Stack.Protected guard={!session}>
+            <Stack.Screen name="login" />
+          </Stack.Protected>
         </Stack>
+       
         <StatusBar style="auto" />
-      </ThemeProvider></GluestackUIProvider>
+      </ThemeProvider>
+    </GluestackUIProvider>
   );
 }
